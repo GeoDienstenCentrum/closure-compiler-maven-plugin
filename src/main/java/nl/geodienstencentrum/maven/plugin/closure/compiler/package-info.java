f0ed617d22558cb8dd55864a641cc9cce1fad08d
Mark Prins <markprins@b3partners.nl>
2015-08-27 14:21:49 +0200

    add package info

---
+++ src/main/java/nl/geodienstencentrum/maven/plugin/closure/compiler/package-info.java
@@ -0,0 +1,16 @@
+/*
+ Copyright 2015 Mark Prins, GeoDienstenCentrum.
+
+ See the README.md file distributed with this work for additional information
+ regarding copyright ownership.
+
+ Licensed under the BSD 3-Clause License (the "License");
+ You may obtain a copy of the License at: http://opensource.org/licenses/BSD-3-Clause
+ */
+/**
+ * Compilers for the Sass maven plugin.
+ *
+ * @author mprins
+ * @since 1.0
+ */
+package nl.geodienstencentrum.maven.plugin.closure.compiler;
